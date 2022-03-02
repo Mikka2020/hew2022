2f787bd5e03ed6a4b73b18ed5ea2449879aae9ec
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-03-03 04:29:11 +0900

    [add]画像にaタグを追加

--- tpl/insect_list.php
+++ tpl/insect_list.php
@@ -14,79 +14,97 @@
   <header></header>
   <main>
     <h1 class="none">昆虫図鑑</h1>
-    <section id="kabutomushi-container">
+    <section>
       <h2>カブトムシ</h2>
       <div>
         <ul>
           <li class="insect-img-container">
-            <img src="./../img/kabutomushi.jpg" alt="カブトムシの画像" width="80px" height="80px">
+            <a href="">
+              <img src="./../img/insects/kabutomushi.jpg" alt="カブトムシの画像" width="80px" height="80px">
+            </a>
           </li>
           <li class="insect-name">カブトムシ</li>
         </ul>
         <ul>
           <li class="insect-img-container">
-            <img src="./../img/kokabutomushi.jpeg" alt="コカブトムシの画像" width="80px" height="80px">
+            <a href="">
+              <img src="./../img/insects/kokabutomushi.jpeg" alt="コカブトムシの画像" width="80px" height="80px">
+            </a>
           </li>
           <li>コカブトムシ</li>
         </ul>
         <ul>
           <li class="insect-img-container">
-            <img src="./../img/saikabuto.jpg" alt="サイカブトの画像" width="80px" height="80px">
+            <a href="">
+              <img src="./../img/insects/saikabuto.jpg" alt="サイカブトの画像" width="80px" height="80px">
+            </a>
           </li>
           <li>サイカブト</li>
         </ul>
       </div>
     </section>
-    <section id="kuwagatamushi-container">
+    <section>
       <h2>クワガタムシ</h2>
       <div>
         <ul>
           <li class="insect-img-container">
-            <img src="./../img/kabutomushi.jpg" alt="カブトムシの画像" width="80px" height="80px">
+            <a href="">
+              <img src="./../img/insects/kabutomushi.jpg" alt="カブトムシの画像" width="80px" height="80px">
+            </a>
           </li>
           <li>カブトムシ</li>
         </ul>
         <ul>
           <li class="insect-img-container">
-            <img src="./../img/kokabutomushi.jpeg" alt="コカブトムシの画像" width="80px" height="80px">
+            <a href="">
+              <img src="./../img/insects/kokabutomushi.jpeg" alt="コカブトムシの画像" width="80px" height="80px">
+            </a>
           </li>
           <li>コカブトムシ</li>
         </ul>
         <ul>
           <li class="insect-img-container">
-            <img src="./../img/saikabuto.jpg" alt="サイカブトの画像" width="80px" height="80px">
+            <a href="">
+              <img src="./../img/insects/saikabuto.jpg" alt="サイカブトの画像" width="80px" height="80px">
+            </a>
           </li>
           <li>サイカブト</li>
         </ul>
         <ul>
           <li class="insect-img-container">
-            <img src="./../img/kabutomushi.jpg" alt="カブトムシの画像" width="80px" height="80px">
+            <a href="">
+              <img src="./../img/insects/kabutomushi.jpg" alt="カブトムシの画像" width="80px" height="80px">
+            </a>
           </li>
           <li>カブトムシ</li>
         </ul>
       </div>
 
     </section>
-    
-    <?php // ユーザーが登録しているむしの一覧を表示 ?>
+
+    <?php // ユーザーが登録しているむしの一覧を表示?>
     <?php for ($i=0; $i < count($insect_list); $i++) { ?>
-      <?php if ($i == 0) { ?>
-      <ul>
+    <?php if ($i == 0) { ?>
+    <ul>
       <li class="insect-img-container">
-          <img src="./../img/<?php echo $insect_list[$i]['insect_photo']; ?>" alt="<?php echo $data_list[$i]['insect_name'] . 'の画像'; ?>"　width="80px" height="80px">
+        <img src="./../img/<?php echo $insect_list[$i]['insect_photo']; ?>"
+          alt="<?php echo $data_list[$i]['insect_name'] . 'の画像'; ?>" width="80px" height="80px">
       </li>
       <li class="insect-img-container">
         <?php echo $data_list[$i]['insect_name']; ?>
       </li>
     </ul>
-      <?php } else { ?>
-        <ul>
+    <?php } else { ?>
+    <ul>
       <li class="insect-img-container">
-          <img src="./../img/<?php echo $insect_list[$i]['insect_photo']; ?>" alt="<?php echo $data_list[$i+1]['insect_name'] . 'の画像'; ?>"　width="80px" height="80px">
+        <img src="./../img/<?php echo $insect_list[$i]['insect_photo']; ?>"
+          alt="<?php echo $data_list[$i+1]['insect_name'] . 'の画像'; ?>" width="80px" height="80px">
       </li>
-      <li class="insect-img-container"><?php echo $data_list[$i+1]['insect_name']; ?></li>
+      <li class="insect-img-container">
+        <?php echo $data_list[$i+1]['insect_name']; ?>
+      </li>
     </ul>
-      <?php } ?>
+    <?php } ?>
     <?php } ?>
   </main>
   <footer></footer>
