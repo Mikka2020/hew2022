08c13a8c7ca974ab6ebfa6dafdaa7bc804f91136
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-03-02 14:22:17 +0900

    [add]CSS読込み

--- tpl/insect_list.php
+++ tpl/insect_list.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/destyle.css">
+    <link rel="stylesheet" href="./insect_list.php">
     <title>Document</title>
 </head>
 <body>
