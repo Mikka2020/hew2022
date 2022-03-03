ab17072bebe42b69661b51b20085b277e5619887
Akiya <hanauchi.aki@gmail.com>
2022-03-03 12:34:14 +0900

    Update insect_list.php
    コメントのスペースを全角から半角に変更

--- model/insect_list.php
+++ model/insect_list.php
@@ -2,7 +2,7 @@
 $db = new PDO('mysql:dbname=hew2022;host=localhost;charset=utf8','root','');
 $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING); // エラー表示をする為に必要
 $result = $db->query("SELECT * FROM `users`;");
-$insect_list = []; //　むしの情報を格納したテーブル
+$insect_list = []; // むしの情報を格納したテーブル
 while($row = $result->fetch(PDO::FETCH_ASSOC)) { // データベースから1件ずつ配列を取得する
     $insect_list[] = $row;
 }
