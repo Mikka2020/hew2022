87d4c28b87a49e82721537a9fb069dcc754a68e6
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-03-05 03:59:44 +0900

    [add]css,header,footer読込み

--- tpl/insect_detail.php
+++ tpl/insect_detail.php
@@ -5,13 +5,18 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./../css/insect_detail.css">
   <title>Document</title>
 </head>
 
 <body>
-  <header></header>
+  <header>
+    <div class="logo"></div>
+  </header>
   <main></main>
-  <footer></footer>
+  <footer>
+    <?php require_once 'footer.php'; ?>
+  </footer>
 </body>
 
 </html>
