88db963a7852142e344c382ee29ee72ff84c4ae7
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-03-06 00:33:29 +0900

    [add]昆虫一覧とログインしているユーザの登録情報を結合した配列をDBから取得する処理を追加

--- model/insect_list.php
+++ model/insect_list.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8',USER_ID,PASSWORD);
 $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING); // エラー表示をする為に必要
 $result = $db->query("SELECT * FROM `registers`;");
@@ -16,3 +17,30 @@ foreach ($insect_list as $key => $value) {
     $data_list[] = $result;
 }
 $db = null; // 終了する
+*/
+
+// テスト用ログイン
+$_COOKIE['user_id'] = 1;
+try {
+    $db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8', USER_ID, PASSWORD);
+    if (isset($_COOKIE['user_id'])) {
+        $stmt = $db->prepare("SELECT * FROM insects LEFT JOIN registers ON insects.insect_id = registers.insect_id AND registers.user_id = :user_id ORDER BY insects.insect_id ASC;");
+        $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $user_id = $_COOKIE['user_id'];
+    } else {
+        $stmt = $db->prepare("SELECT * FROM insects;");
+    }
+
+    $stmt->execute();
+    // むしの配列
+    $insect_list = [];
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $insect_list[] = $row;
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+} catch (Exception $e) {
+    echo $e->getMessage();
+} finally {
+    $db = null;
+}
