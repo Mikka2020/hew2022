9bdb8b0464df9d7b1566f2dbb13d787551c26ed0
Akiya <hanauchi.aki@gmail.com>
2022-03-05 20:39:16 +0900

    Update insect_list.php
    テーブルの変更。
    WHERE句の変更

--- model/insect_list.php
+++ model/insect_list.php
@@ -1,7 +1,7 @@
 <?php
 $db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8',USER_ID,PASSWORD);
 $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING); // エラー表示をする為に必要
-$result = $db->query("SELECT * FROM `users`;");
+$result = $db->query("SELECT * FROM `registers`;");
 $insect_list = []; // むしの情報を格納したテーブル
 while($row = $result->fetch(PDO::FETCH_ASSOC)) { // データベースから1件ずつ配列を取得する
     $insect_list[] = $row;
@@ -9,7 +9,7 @@ while($row = $result->fetch(PDO::FETCH_ASSOC)) { // データベースから1件
 
 $data_list = [];
 foreach ($insect_list as $key => $value) {
-    $result = $db->query("SELECT * FROM `insects` WHERE insect_name_ro = '" . $value['insect_name'] . "' ;");
+    $result = $db->query("SELECT * FROM `insects` WHERE insect_id = '" . $value['insect_id'] . "' ;");
     while($row = $result->fetch(PDO::FETCH_ASSOC)) { // データベースから1件ずつ配列を取得する
         $data_list[] = $row;
     }
