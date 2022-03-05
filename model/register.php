dd3c6f7c66bab6349f38d278e8a0ef4459d22bcc
Akiya <hanauchi.aki@gmail.com>
2022-03-05 14:56:58 +0900

    registerテーブルに値を格納する処理

--- model/register.php
+++ model/register.php
@@ -1,1 +1,5 @@
 <?php
+// registersテーブルに投稿情報を格納する
+$db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8',USER_ID,PASSWORD);
+$db->exec("INSERT INTO 'registers' ('register_id' , 'user_id' , 'insect_id' , 'registered_at' , 'taked_at') VALUES ('" . $register_id . "','" . $user_id . "','" . $result[0]['index'] . "','" . NULL . "','" . NULL . "')");
+$db = null; // 終了
