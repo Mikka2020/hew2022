d00986a62f51e082fa24caceb219f00d59e99966
Akiya <hanauchi.aki@gmail.com>
2022-03-05 13:48:58 +0900

    値の初期化とデータ取得
    格納する値を受け取る前に初期化する。
    usersテーブルに格納されている値を配列に全件取得

--- model/login.php
+++ model/login.php
@@ -1,6 +1,9 @@
 <?php
-// 入力された値をusersテーブルに格納していく
-
+// 初期化
+$_POST['login_id'] = '';
+$_POST['password'] = '';
+$_POST['mail'] = '';
+$_POST['birth_day'] = '';
 /** 格納する値
  * $_POST['login_id'];
  * $_POST['password'];
@@ -8,13 +11,12 @@
  * $_POST['birth_day'];
  */
 
-try {
-    $db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8',USER_ID,PASSWORD);
-    $db->exec("INSERT INTO 'users' ('login_id' , 'password' , ' mail' , 'birth_day') VALUES ('" . $_POST['login_id'] . "','" . $_POST['password'] . "','" . $_POST['mail'] . "','" . $_POST['birth_day'] . "')");
-} catch (PDOException $e) {
-    echo $e->getMessage();
-} catch (Exception $e) {
-    echo $e->getMessage();
-} finally {
-    $db = null;
+// usersテーブルに値を格納し全件取得する
+$db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8',USER_ID,PASSWORD);
+$db->exec("INSERT INTO 'users' ('login_id' , 'password' , ' mail' , 'birth_day') VALUES ('" . $_POST['login_id'] . "','" . $_POST['password'] . "','" . $_POST['mail'] . "','" . $_POST['birth_day'] . "')");
+$result = $db->query("SELECT * FROM `users`;");
+$user_data_table = []; // userテーブルに格納されたいる全データを取得
+while($row = $result->fetch(PDO::FETCH_ASSOC)) { // データベースから1件ずつ配列を取得する
+    $user_data_table[] = $row;
 }
+$db = null; // 終了
