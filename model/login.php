d1700c794c9443be9bc007f217e1c70b32999cd7
Akiya <hanauchi.aki@gmail.com>
2022-03-05 13:31:57 +0900

    usersテーブルに値を格納
    現在は入力値チェックや、ログインID
    、パスワードの重複は考えていない

--- model/login.php
+++ model/login.php
@@ -8,3 +8,13 @@
  * $_POST['birth_day'];
  */
 
+try {
+    $db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8',USER_ID,PASSWORD);
+    $db->exec("INSERT INTO 'users' ('login_id' , 'password' , ' mail' , 'birth_day') VALUES ('" . $_POST['login_id'] . "','" . $_POST['password'] . "','" . $_POST['mail'] . "','" . $_POST['birth_day'] . "')");
+} catch (PDOException $e) {
+    echo $e->getMessage();
+} catch (Exception $e) {
+    echo $e->getMessage();
+} finally {
+    $db = null;
+}
