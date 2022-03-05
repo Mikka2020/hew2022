a30a4755b4bb43818e2b98167c58c8678d535ec3
Akiya <hanauchi.aki@gmail.com>
2022-03-06 02:02:42 +0900

    Update insect_detail.php
    詳細画面の表示

--- tpl/insect_detail.php
+++ tpl/insect_detail.php
@@ -6,14 +6,26 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./../css/insect_detail.css">
-  <title>Document</title>
+  <title>詳細画面</title>
 </head>
 
 <body>
   <header>
     <div class="logo"></div>
   </header>
-  <main></main>
+  <main>
+    <?php foreach ($insect_data as $row) { ?>
+      がくめい <?php echo $row['insect_scientific_name']; ?>
+      <br>
+      ぶんるい <?php echo $row['insect_moku']; ?>目
+      <?php echo $row['insect_amoku']; ?>亜目
+      <?php echo $row['insect_ka']; ?>科
+      <?php echo $row['insect_aka']; ?>亜科
+      <br>
+      レア度 <?php for ($i=0; $i < $row['rarity'] ; $i++) { ?><?php echo ' ★ '; ?><?php } ?>
+      <?php for ($i=0; $i < $rea; $i++) { ?><?php echo ' ☆ '; ?><?php } ?>
+    <?php } ?>
+  </main>
   <footer>
     <?php require_once 'footer.php'; ?>
   </footer>
