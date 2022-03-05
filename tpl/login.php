3803b6a482a31e17e90dcc46b500a0427e5b605a
Akiya <hanauchi.aki@gmail.com>
2022-03-05 20:28:54 +0900

    Create login.php
    ログイン画面の作成

---
+++ tpl/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./../css/insect_detail.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <header>
+    <div class="logo"></div>
+  </header>
+  <main></main>
+  <footer>
+    <?php require_once 'footer.php'; ?>
+  </footer>
+</body>
+
+</html>
