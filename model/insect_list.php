36c58fd0b7f3cd74201e2cf7b560a406245a5ef1
Akiya <hanauchi.aki@gmail.com>
2022-03-06 21:21:23 +0900

    Update insect_list.php
    獲得している経験値に応じてレベルを変更

--- model/insect_list.php
+++ model/insect_list.php
@@ -24,3 +24,51 @@ try {
 } finally {
     $db = null;
 }
+
+// ユーザーがクリアしてるクエストの獲得経験値に応じてレベルを変動
+$user_level = 0; // ユーザーの初期レベル
+try {
+    $db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8', USER_ID, PASSWORD);
+    $stmt = $db->prepare("SELECT * FROM cleared_quests INNER JOIN quests ON cleared_quests.cleared_quest_id = quests.quest_id AND cleared_quests.user_id = :user_id;");
+    $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $user_id = $_COOKIE['user_id'];
+    $stmt->execute();
+    // 獲得した経験値の配列
+    $quest_exp = [];
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $quest_exp[] += $row['exp'];
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+} catch (Exception $e) {
+    echo $e->getMessage();
+} finally {
+    $db = null;
+}
+
+$sum_exp = 0; // 獲得経験値の合計値
+foreach ($quest_exp as $row) {
+    $sum_exp += $row;
+}
+
+if (0 <= $sum_exp && $sum_exp < 3) { // 0以上3以下の場合はレベル1
+    $user_level = 1;
+} else if (3 <= $sum_exp && $sum_exp < 10) { // 3以上10以下の場合はレベル2
+    $user_level = 2;
+} else if (10 <= $sum_exp && $sum_exp < 18) { // 10以上18未満の場合はレベル3
+    $user_level = 3;
+} else if (18 <= $sum_exp && $sum_exp < 26) { // 18以上26未満の場合はレベル4
+    $user_level = 4;
+} else if (26 <= $sum_exp && $sum_exp < 42) { // 26以上42未満の場合はレベル5
+    $user_level = 5;
+} else if (42 <= $sum_exp && $sum_exp < 57) { // 42以上57未満の場合はレベル6
+    $user_level = 6;
+} else if (57 <= $sum_exp && $sum_exp < 77) { // 57以上77未満の場合はレベル7
+    $user_level = 7;
+} else if (77 <= $sum_exp && $sum_exp < 100) { // 77以上100未満の場合はレベル8
+    $user_level = 8;
+} else if (100 <= $sum_exp && $sum_exp < 125) { // 100以上125未満の場合はレベル9
+    $user_level = 9;
+} else if (125 <= $sum_exp && $sum_exp < 157) { // 125以上157未満の場合はレベル10
+    $user_level = 10;
+}
