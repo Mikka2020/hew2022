48ae00bc9b5a41369cc36eca1aeeafef1f55e897
hogehogeBrav <zmzmakoto@icloud.com>
2022-03-07 03:34:11 +0900

    [update] 画像保存ファイル名にinsect_idと枚数を追加

--- model/register.php
+++ model/register.php
@@ -1,7 +1,12 @@
 <?php
 
+//元からある画像を取得
+$folder = glob('./users/' . $user_id . '/' .$user_id. '_' . $insect_id . '_{*.jpg,*.jpeg,*.png,*.gif,*.JPG,*.JPEG,*.PNG,*.GIF}', GLOB_BRACE);
+//元から存在する画像数
+$photo_cnt = count($folder);
+
 //ユーザーIDのフォルダに格納(ユーザID_現在時刻.jpg)
-copy('./python/tmp/' . $file_name , '../users/' .$user_id. '/'. $user_id . '_' . date('YmdHis') . '.' . $ext);
+copy('./python/tmp/' . $file_name , '../users/' .$user_id. '/'. $user_id . '_' . $insect_id . '_' . $photo_cnt++ . '.' . $ext);
 
 // registersテーブルに投稿情報を格納する
 $db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8', USER_ID ,PASSWORD);
