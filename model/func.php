f0961fc585f2b071935e896bec78a83db48dd9c8
Akiya <hanauchi.aki@gmail.com>
2022-03-07 01:45:05 +0900

    関数のコメントを記述

--- model/func.php
+++ model/func.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * 引数１：ユーザーID
+ * 処理：引数1に入っているユーザーIDがどのむしを登録しているのかを配列true,falseで返す
+ * true:登録済み
+ * false:未登録
+ */
 function all_insect_check($user_id) {
     $db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8', USER_ID, PASSWORD);
     $stmt = $db->prepare("SELECT * FROM registers INNER JOIN insects ON registers.insect_id = insects.insect_id AND registers.user_id = $user_id ORDER BY insects.insect_id ASC;");
@@ -17,6 +23,13 @@ function all_insect_check($user_id) {
     return $all_check;
 }
 
+/**
+ * 引数1:ユーザーID
+ * 引数2:むしのID
+ * 処理:引数1に入ってるユーザーIDが引数2に入っているむしのIDを登録している場合はtrue,登録していない場合はfalseを返す
+ * true:登録ずみ
+ * false:未登録
+ */
 function insect_check($user_id,$insect_id) {
     $db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8', USER_ID, PASSWORD);
     $stmt = $db->prepare("SELECT * FROM registers INNER JOIN insects ON registers.insect_id = insects.insect_id AND registers.user_id = $user_id ORDER BY insects.insect_id ASC;");
