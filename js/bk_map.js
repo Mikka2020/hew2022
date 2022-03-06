dd50828cc39828940f1a8af251e7f67b7c98ae86
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-03-06 21:56:00 +0900

    [add]mapをクリックで拡大する処理のファイルをバックアップ

--- js/bk_map.js
+++ js/bk_map.js
@@ -1,97 +1,107 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    svgJapan({
-      element: "#my-map-container",
-      // type: "full",
-      type: "deform",
-      // activeColor: "",
-      withTips: "",
-      regionality: "true",
-      width: "100%",
-    });
-    document.getElementById("strokes").style.display = "none";
-  },
-  false
-);
-
-// 地方をクリックしたときの関数
-// function clickRegionFunc() {}
-document.addEventListener(
-  "svgmap.click",
-  function clickRegionFunc(e) {
-    const clickedRegion = e.target.getAttribute("data-region");
-    console.log(clickedRegion);
-    console.log(screen.width);
-    const clickedPrefsArr = [
-      [1],
-      [2, 3, 4, 5, 6, 7],
-      [8, 9, 10, 11, 12, 13, 14],
-      [15, 16, 17, 18, 19, 20, 21, 22, 23],
-      [24, 25, 26, 27, 28, 29, 30],
-      [31, 32, 33, 34, 35],
-      [36, 37, 38, 39],
-      [40, 41, 42, 43, 44, 45, 46, 47],
-    ];
-    console.log(document.getElementById("my-map-container"));
-    // スマホサイズのとき
-    if (screen.width < 480) {
-      // 拡大
-      // document.getElementById("container").style.height = "600px";
-      document.getElementById("my-map-container").style.height = "780px";
-      document.getElementById("my-map-container").style.width = "200%";
+const prefsArr = [
+  [1],
+  [2, 3, 4, 5, 6, 7],
+  [8, 9, 10, 11, 12, 13, 14],
+  [15, 16, 17, 18, 19, 20, 21, 22, 23],
+  [24, 25, 26, 27, 28, 29, 30],
+  [31, 32, 33, 34, 35],
+  [36, 37, 38, 39],
+  [40, 41, 42, 43, 44, 45, 46, 47],
+];
+const myMapContainer = document.getElementById("map-container");
+// 地方をクリックしたとき
+$(function () {
+  $(".prefecture-map").on("click", (e) => {
+    // クリックした都道府県のid
+    const clickedPrefId = e.target.getAttribute("id").replace("pref-", "");
+    $(`#pref-${clickedPrefId}`).on("click", handleClickRegion(e));
+    // 地方をクリックしたときの関数
+    function handleClickRegion(e) {
+      // クリックされた地方
+      const clickedRegionId = e.target.getAttribute("data-region");
+      console.log(clickedRegionId);
+      // スマホとパソコンで表示処理を切り替え
+      // if (screen.width < 480) {
+      // }
+      // スマホサイズのとき
+      myMapContainer.style.height = "180vw";
+      myMapContainer.style.width = "180vw";
 
       // 選択した地方が画面の中央になるように位置を変更
-      if (clickedRegion == 1) {
-        document.getElementById("my-map-container").style.top = "10%";
-        document.getElementById("my-map-container").style.right = "20%";
-      } else if (clickedRegion == 2) {
-        document.getElementById("my-map-container").style.top = "-40%";
-        document.getElementById("my-map-container").style.right = "18%";
-      } else if (clickedRegion == 3) {
-        document.getElementById("my-map-container").style.bottom = "-20%";
-        document.getElementById("my-map-container").style.right = "12%";
-      } else if (clickedRegion == 4) {
-        document.getElementById("my-map-container").style.bottom = "-30%";
-        document.getElementById("my-map-container").style.right = "-15%";
-      } else if (clickedRegion == 5) {
-        document.getElementById("my-map-container").style.bottom = "-20%";
-        document.getElementById("my-map-container").style.right = "-50%";
-      } else if (clickedRegion == 6) {
-        document.getElementById("my-map-container").style.bottom = "-30%";
-        document.getElementById("my-map-container").style.left = "-20%";
-      } else if (clickedRegion == 7) {
-        document.getElementById("my-map-container").style.bottom = "0%";
-        document.getElementById("my-map-container").style.left = "-20%";
-      } else if (clickedRegion == 8) {
-        document.getElementById("my-map-container").style.bottom = "-8%";
-        document.getElementById("my-map-container").style.left = "18%";
+      function locateRegionCenter(clickedRegionId) {
+        if (clickedRegionId == 1) {
+          // myMapContainer.css({ top: "10%", right: "20%" });
+          myMapContainer.style.top = "10%";
+          myMapContainer.style.right = "20%";
+        } else if (clickedRegionId == 2) {
+          myMapContainer.style.top = "-40%";
+          myMapContainer.style.right = "18%";
+        } else if (clickedRegionId == 3) {
+          myMapContainer.style.bottom = "-20%";
+          myMapContainer.style.right = "12%";
+        } else if (clickedRegionId == 4) {
+          myMapContainer.style.bottom = "-30%";
+          myMapContainer.style.right = "-15%";
+        } else if (clickedRegionId == 5) {
+          myMapContainer.style.bottom = "-20%";
+          myMapContainer.style.right = "-50%";
+        } else if (clickedRegionId == 6) {
+          myMapContainer.style.bottom = "-30%";
+          myMapContainer.style.left = "-20%";
+        } else if (clickedRegionId == 7) {
+          myMapContainer.style.bottom = "0%";
+          myMapContainer.style.left = "-20%";
+        } else if (clickedRegionId == 8) {
+          myMapContainer.style.bottom = "-8%";
+          myMapContainer.style.left = "18%";
+        } else {
+          // パソコンサイズの表示処理
+        }
       }
-
-      //
-      document.removeEventListener("svgmap.click", clickRegionFunc);
-
-      // 都道府県をクリックしたときの関数
-    }
-
-    // 選択した都道府県が含まれた地方以外の地方を非表示にする
-    (() => {
-      for (let i = 1; i <= 48; i++) {
-        if (
-          i >= clickedPrefsArr[clickedRegion - 1][0] &&
+      locateRegionCenter(clickedRegionId);
+      // 選択した都道府県が地方に含まれているか
+      function isClickedRegion(i) {
+        return (
+          i >= prefsArr[clickedRegionId - 1][0] &&
           i <=
-            clickedPrefsArr[clickedRegion - 1][
-              clickedPrefsArr[clickedRegion - 1].length - 1
+            prefsArr[clickedRegionId - 1][
+              prefsArr[clickedRegionId - 1].length - 1
             ]
-        ) {
+        );
+      }
+      // 選択した都道府県が含まれた地方以外の地方を非表示にする処理
+      for (let i = 1; i <= 48; i++) {
+        if (isClickedRegion(i)) {
+          // 押していない都道府県に関数を付与
+          // if (clickedPrefId != i) {
+          // $(`#pref-${i}`).on("click", clickRegionFunc(e));
+          // continue;
+          // console.log(1);
+          // }
           continue;
         }
-        document.getElementById("pref-" + i).style.display = "none";
+        $(`#pref-${i}`).attr("display", "none");
       }
-    })();
 
-    //
-    // (() => {})();
-  },
-  false
-);
+      $(`#pref-${clickedPrefId}`).off("click", handleClickRegion);
+      // 地方の処理が終わったので都道府県の処理を記述する
+
+      for (i = 0; i < prefsArr[clickedRegionId - 1].length; i++) {
+        $(`#pref-${parseInt(clickedPrefId) + i}`).on("click", clickPrefFunc);
+      }
+      // $(`#pref-${clickedPrefId + 1}`).one("click", clickPrefFunc);
+
+      // 都道府県を拡大
+      function clickPrefFunc() {
+        for (let i = 1; i <= 48; i++) {
+          if (i == clickedPrefId) {
+            // console.log(clickedPrefId);
+            continue;
+          }
+          $(`#pref-${i}`).attr("display", "none");
+        }
+      }
+      // console.log(e.target.getAttribute("id").replace("pref-", ""));
+    }
+  });
+});
