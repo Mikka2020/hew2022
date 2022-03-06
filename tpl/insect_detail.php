6c674d485592030590c960ce09d7be39ea999ab3
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-03-06 11:03:12 +0900

    [add]新規作成

--- tpl/insect_detail.php
+++ tpl/insect_detail.php
@@ -11,20 +11,62 @@
 
 <body>
   <header>
-    <div class="logo"></div>
+    <p>カブトムシ</p>
   </header>
   <main>
-    <?php foreach ($insect_data as $row) { ?>
-      がくめい <?php echo $row['insect_scientific_name']; ?>
-      <br>
-      ぶんるい <?php echo $row['insect_moku']; ?>目
-      <?php echo $row['insect_amoku']; ?>亜目
-      <?php echo $row['insect_ka']; ?>科
-      <?php echo $row['insect_aka']; ?>亜科
-      <br>
-      レア度 <?php for ($i=0; $i < $row['rarity'] ; $i++) { ?><?php echo ' ★ '; ?><?php } ?>
-      <?php for ($i=0; $i < $rea; $i++) { ?><?php echo ' ☆ '; ?><?php } ?>
-    <?php } ?>
+    <div class="insect-img-container">
+      <img src="./../img/insects/kabutomushi.jpg" alt="カブトムシの画像">
+    </div>
+    <ul class="contents-tabs">
+      <li class="active">がいよう</li>
+      <li>せつめい</li>
+      <li>まめちしき</li>
+    </ul>
+    <section>
+      <?php foreach ($insect_data as $row) : ?>
+      <dl>
+        <div class="data-1">
+          <dt>がくめい</dt>
+          <dd>
+            <?php echo $row['insect_scientific_name']; ?>
+          </dd>
+        </div>
+        <div class="data-1">
+          <dt>ぶんるい</dt>
+          <div>
+            <dd>
+              <?php echo $row['insect_moku']; ?>目
+            </dd>
+            <dd>
+              <?php echo $row['insect_amoku']; ?>亜目
+            </dd>
+            <dd>
+              <?php echo $row['insect_ka']; ?>科
+            </dd>
+            <dd>
+              <?php echo $row['insect_aka']; ?>亜科
+            </dd>
+          </div>
+        </div>
+        <div class="data-1">
+          <dt>レア度</dt>
+          <dd>
+            <?php
+              for ($i=0; $i < $row['rarity'] ; $i++) {
+                  echo ' ★ ';
+              }
+            ?>
+            <?php
+              for ($i=0; $i < $rea; $i++) {
+                  echo ' ☆ ';
+              }
+            ?>
+          </dd>
+        </div>
+      </dl>
+      <?php endforeach; ?>
+    </section>
+
   </main>
   <footer>
     <?php require_once 'footer.php'; ?>
