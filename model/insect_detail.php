563b69a44ffd2dbd7b4e180eb4b5908fd077091c
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-03-06 13:12:24 +0900

    [update]GETで取得したむしidの情報をDBから取得

--- model/insect_detail.php
+++ model/insect_detail.php
@@ -1,12 +1,17 @@
 <?php
 // 昆虫IDを取得しinsectsテーブルから全件取得
-$db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8',USER_ID,PASSWORD);
-$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING); // エラー表示をする為に必要
-$result = $db->query("SELECT * FROM `insects` WHERE insect_id = 42;");
+$db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8', USER_ID, PASSWORD);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // エラー表示をする為に必要
+// $result = $db->query("SELECT * FROM `insects` WHERE insect_id = 42;");
+$stmt = $db->prepare("SELECT * FROM `insects` WHERE insect_id = :id"); // ゲットで取得したidのむしを取得
+$stmt->bindParam(':id', $insect_id, PDO::PARAM_INT);
+$insect_id = $_GET['id'];
+$stmt->execute();
 $insect_data = []; // むしの情報を格納したテーブル
-while($row = $result->fetch(PDO::FETCH_ASSOC)) { // データベースから1件ずつ配列を取得する
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { // データベースから1件ずつ配列を取得する
     $insect_data[] = $row;
 }
+$db = null;
 
 foreach ($insect_data as $key => $value) {
     # code...
