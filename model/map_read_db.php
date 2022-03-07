5c86cd27c7a66f1444be6d03f6310a43d7dad68f
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-03-08 05:40:58 +0900

    [update]dbからextを取得

--- model/map_read_db.php
+++ model/map_read_db.php
@@ -4,7 +4,7 @@ require_once "./../../config.php";
 try {
     $db = new PDO('mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=utf8', USER_ID, PASSWORD);
     if (isset($_POST['user_id'])) {
-        $stmt = $db->prepare("SELECT insects.insect_id AS insects_insect_id,insects.insect_name,insects.pref_id,registers.id,registers.register_id,prefs.pref_id FROM insects INNER JOIN prefs ON insects.pref_id = prefs.pref_id INNER JOIN insect_categories ON insects.insect_category_id = insect_categories.category_id LEFT JOIN registers ON insects.insect_id = registers.insect_id AND registers.user_id = :user_id GROUP BY insects.insect_id ORDER BY insects.insect_id ASC;");
+        $stmt = $db->prepare("SELECT insects.insect_id AS insects_insect_id, insects.insect_name, insects.pref_id, registers.id, registers.register_id,prefs.pref_id, registers.ext FROM insects INNER JOIN prefs ON insects.pref_id = prefs.pref_id INNER JOIN insect_categories ON insects.insect_category_id = insect_categories.category_id LEFT JOIN registers ON insects.insect_id = registers.insect_id AND registers.user_id = :user_id GROUP BY insects.insect_id ORDER BY insects.insect_id ASC;");
         $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
         $user_id = $_POST['user_id'];
     } else {
