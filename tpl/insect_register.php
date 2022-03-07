5bda23e1bf7ded75c75ab1e82496aa5b0bbdcc63
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-03-07 09:08:02 +0900

    [add]選択した画像を表示する機能を追加

--- tpl/insect_register.php
+++ tpl/insect_register.php
@@ -5,18 +5,37 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./../css/insect_detail.css">
-  <title>Document</title>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <link rel="stylesheet" href="./../css/insect_register.css">
+  <title>とうろく</title>
 </head>
 
 <body>
   <header>
-    <div class="logo"></div>
-  </header>
-  <main></main>
+   <p>とうろく</p>
+   </header>
+  <main>
+    <form action="" method="post" enctype="multipart/form-data">
+      <section id="select-container">
+        <label>
+          <input type="file" name="insect_img" class="input-img" id="myImage" accept="image/*">
+          <img id="preview">
+        </label>
+      </section>
+      <section id="text-container">
+        <h2>つかいかた</h2>
+        <p>むしの写真をえらぶと自動でむしの名前を教えてくれます！</p>
+        <p>はんていした写真をずかんにとうろくできるよ！</p>
+        <div class="btn-container">
+          <button type="submit">はんてい</button>
+        </div>
+      </section>
+    </form>
+  </main>
   <footer>
     <?php require_once 'footer.php'; ?>
   </footer>
+  <script src="./../js/insect_register.js"></script>
 </body>
 
 </html>
