e4630e0bfbf1c288c6e5d767fdd77e96cd9fd27c
hogehogeBrav <zmzmakoto@icloud.com>
2022-03-07 23:36:16 +0900

    [update] insect_register_result.php

--- tpl/insect_register_result.php
+++ tpl/insect_register_result.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./../sass/insect_register_result.css">
+  <link rel="stylesheet" href="./../css/insect_register_result.css">
   <title>とうろく</title>
 </head>
 
