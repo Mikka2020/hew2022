3f273dc9d0cf941be42e9ff88d37b40b95334019
komaki216 <komaki.0025@gmail.com>
2022-03-08 09:08:08 +0900

    [update]ホーム画面CSS更新

--- tpl/home.php
+++ tpl/home.php
@@ -16,9 +16,65 @@
   <header>
     <p>ホーム</p>
   </header>
+  
   <main>
+    <article class="user-area">
+      <div class="rank">
+        <div class="rank-background"></div>
+        <div class="rank-center">
+          <p>Rank</p>
+          <p>12</p>
+        </div>
+      </div>
+      <ul>
+        <li class="user-title">むしはかせ</li>
+        <li class="user-name">かわしま</li>
+        <li class="rank-count">つぎのランクまで40</li>
+      </ul>
+    </article>
 
+    <article class="link-area">
+      <section>
+        <div id="newsbtn"><a href=""></a></div>
+        <p>おしらせ</p>
+      </section>
+      <section>
+        <div id="questbtn"><a href="./quest.php"></a></div>
+        <p>クエスト</p>
+      </section>
+      <section>
+        <a href=""><div id="eventbtn"></div></a>
+        <p>イベント</p>
+      </section>
+    </article>
+
+    <article class="quest-area">
+      <ul class="sort-tab">
+        <li class="active">ウィークリー</li>
+        <li>メイン</li>
+        <li>イベント</li>
+      </ul>
+      <div class="quest">
+        <section>
+          <div class=""></div>
+          <p>むしを３匹とうろくしてみよう！</p>
+          <ul>
+            <li>経験値 10</li>
+            <li>期限 なし</li>
+          </ul>
+        </section>
+        <section>
+          <div class=""></div>
+          <p>むしを３匹とうろくしてみよう！</p>
+          <ul>
+            <li>経験値 10</li>
+            <li>期限 なし</li>
+          </ul>
+        </section>
+      </div>
+    </article>
   </main>
+
   <footer>
     <?php require_once './footer.php'; ?>
   </footer>
